b3736bf9db7b6de86c84e993daa07194e4c4e5e0
hull <hull@e4a6332b-da94-4e19-b8c2-16eed22ecab5>
2011-06-17 11:45:57 +0000

    Docs for Transition, OnEvent and OnCondition
    git-svn-id: http://svn.incf.org/svn/nineml/branches/model-tree@276 e4a6332b-da94-4e19-b8c2-16eed22ecab5

---
+++ lib9ml/python/nineml/abstraction_layer/component/util.py
@@ -0,0 +1,31 @@
+
+from expressions import Alias, ODE
+import re
+
+class StrToExpr(object):
+
+
+    @classmethod
+    def is_alias(cls, alias_string):
+        return ':=' in alias_string
+
+    @classmethod
+    def alias(cls, alias_string):
+        if not cls.is_alias(alias_string):
+            errmsg = "Invalid Alias: %s"%alias_string
+            raise NineMLRuntimeException(errmsg)
+
+        lhs,rhs = alias_string.split(':=')
+        return Alias( lhs = lhs.strip(), rhs = rhs.strip() )
+
+
+    @classmethod
+    def time_derivative(cls, time_derivative_string):
+        r = re.compile(r"""\s* d(?P<var>[a-zA-Z][a-zA-Z0-9_]*)/dt \s* = \s* (?P<rhs> .*) """, re.VERBOSE) 
+        m = r.match(time_derivative_string)
+        return ODE( dependent_variable = m.groupdict()['var'], indep_variable='t', rhs=m.groupdict()['rhs'] )
+
+
+    @classmethod
+    def state_assignment(cls, state_assignment_string):
+        assert False
