7f87060ed4a69ce3fa4b5333cd44be455c6cf5b6
andrewdavison <andrewdavison@e4a6332b-da94-4e19-b8c2-16eed22ecab5>
2011-07-26 14:09:52 +0000

    setup.py for nineml Python package now seems to be working
    git-svn-id: http://svn.incf.org/svn/nineml/trunk@497 e4a6332b-da94-4e19-b8c2-16eed22ecab5

--- lib9ml/python/setup.py
+++ lib9ml/python/setup.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+#from distutils.core import setup
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
 from nineml.__init__ import __version__
-      
+
 setup(
     name = "9ML",
     version = __version__,
-    packages = ['nineml', 'nineml.abstraction_layer'],
-    author = "Andrew P. Davison", # add your name here if you contribute to the code
+    packages = ['nineml',
+                'nineml.abstraction_layer',
+                'nineml.abstraction_layer.component',
+                'nineml.abstraction_layer.component.parse',
+                'nineml.abstraction_layer.component_modifiers',
+                'nineml.abstraction_layer.flattening',
+                'nineml.abstraction_layer.readers',
+                'nineml.abstraction_layer.testing_utils',
+                'nineml.abstraction_layer.validators',
+                'nineml.abstraction_layer.visitors',
+                'nineml.abstraction_layer.writers',
+                'nineml.exceptions',
+                'nineml.maths',
+                'nineml.utility'
+                ],
+    package_data = {'nineml': ['examples/AL/demos/*.py', "examples/AL/components_done/*.py"]},
+    #packages = find_packages(),
+    author = "Andrew P. Davison, Eilif Muller, Mike Hull", # add your name here if you contribute to the code
     author_email = "andrewpdavison@gmail.com",
-    description = "A tool reading, writing and generally working with 9ML files.",
+    description = "A tool for reading, writing and generally working with 9ML files.",
+    long_description = open("README").read(),
     license = "BSD 3 License",
     keywords = "computational neuroscience modeling interoperability XML",
     url = "http://nineml.incf.org",
-    classifiers = ['Development Status :: 2 - Pre-Alpha',
+    classifiers = ['Development Status :: 3 - Alpha',
                    'Environment :: Console',
                    'Intended Audience :: Science/Research',
                    'License :: OSI Approved :: BSD License',
@@ -21,6 +41,7 @@ setup(
                    'Operating System :: OS Independent',
                    'Programming Language :: Python :: 2',
                    'Topic :: Scientific/Engineering'],
-    requires = ['lxml'],
+    install_requires = ['lxml', 'ply'],
+    tests_require = ['nose']
 )
 
