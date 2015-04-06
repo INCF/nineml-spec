5522d4d6b7e2b4c1e4b2e03bf7366c57db3c4c45
Tom Close <tclose@oist.jp>
2015-04-06 11:35:39 +0900

    changed name of specification filename

--- spec/latexdiff.sh
+++ spec/latexdiff.sh
@@ -5,5 +5,5 @@ fi
 orig_dir=`pwd`
 cd `dirname $0`
 mkdir -p changes
-git latexdiff $1 --main specification.tex --tmpdirprefix `pwd`/changes --exclude-textcmd="section,subsection,subsubsection,subsubsubsection,part" --exclude-safecmd="ref"
+git latexdiff $1 --main NineMLSpec.tex --tmpdirprefix `pwd`/changes --exclude-textcmd="section,subsection,subsubsection,subsubsubsection,part" --exclude-safecmd="ref"
 cd $orig_dir
