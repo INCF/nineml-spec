2180bd18e941fee5f1fefc3909c958d868df9f93
Tom Close <tom.g.close@gmail.com>
2017-06-30 22:15:29 +1000

    preparing for sphinx build

--- latex/run_latexdiff.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if [ $# -lt 1 ]; then
-    error "Must provide revision SHA"
-fi
-orig_dir=`pwd`
-cd `dirname $0`
-mkdir -p changes
-git latexdiff $1 --main NineMLSpec.tex --tmpdirprefix `pwd`/changes --exclude-textcmd="section,subsection,subsubsection,subsubsubsection,part" --exclude-safecmd="ref"
-cd $orig_dir
