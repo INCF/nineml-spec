dcfdbc7636d4744deb5dd065ab8990c4711e4a48
Tom Close <tom.g.close@gmail.com>
2017-12-09 18:46:28 +1100

    cleaned up indentation in conf.py

--- source/conf.py
+++ source/conf.py
@@ -27,8 +27,8 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.todo',
-   			  'sphinx.ext.mathjax',
-    		  'sphinx.ext.autosectionlabel']
+              'sphinx.ext.mathjax',
+              'sphinx.ext.autosectionlabel']
 
 
 # Add any paths that contain templates here, relative to this directory.
